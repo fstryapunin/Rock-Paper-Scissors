5afcfb7727a0917efe259ae3c752b57248dac54c
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2021-01-11 12:18:13 +0100

    added main view and tis sub elements

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <div className="main-view">
+            <h1>Rock, paper, scissors & more</h1>
+            <div>
+                <button className="main-view-button">Start</button>
+                <button className="main-view-button">Options</button>
+            </div>
+        </div>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js'
 import './index.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <div></div>
+    <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
